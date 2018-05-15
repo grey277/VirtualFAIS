260fd53edb2f15b61b20790cd3c4748e636bfa61
Kamil Rakoczy <grey277@wp.pl>
2018-05-16 00:52:56 +0200

    Add room object o popup activity

--- app/src/main/java/com/grey/virtualfais/PopupActivity.java
+++ app/src/main/java/com/grey/virtualfais/PopupActivity.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.grey.virtualfais.daos.RoomDao;
+import com.grey.virtualfais.models.Room;
+import com.grey.virtualfais.services.AppDatabase;
+
 public class PopupActivity extends Activity {
+
+    static private RoomDao roomDao;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,5 +20,9 @@ public class PopupActivity extends Activity {
 
         getWindow().setLayout(200, 300);
         String room_id = getIntent().getStringExtra("room_id");
+        AppDatabase appDatabase = AppDatabase.getInstance(this);
+
+        roomDao = appDatabase.roomDao();
+        Room room = roomDao.getByRoomId(room_id);
     }
 }
